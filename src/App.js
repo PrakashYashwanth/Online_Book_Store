8c1a64ce1c8cc431cec7a1896791c562024bc3e8
prakash_yash <myashwanth096@gmail.com>
2023-03-19 21:26:25 +0530

    feature: adding react and react dom

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Online Book Store</h1>
+        <p>
+          An app for selling and buying books with user friendly interface
+          compatible for all divices
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
