22039d2115653c8c110da99bd92561a7ac115843
prakash_yash <myashwanth096@gmail.com>
2023-04-03 19:27:56 +0530

    loading data of books in home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,16 +2,33 @@ import React, { useContext, useEffect, useState } from "react";
 import { BookContext } from "../../store/contextStore";
 import { useNavigate } from "react-router-dom";
 import { Box, CircularProgress } from "@mui/material";
+import axios from "axios";
 
 const Home = () => {
   const [state] = useContext(BookContext);
   const [isLoading, setIsLoading] = useState(true);
+  const [booksData, setBooksData] = useState([]);
   const navigate = useNavigate();
+
+  const getBooksData = async () => {
+    try {
+      const res = await axios.get(
+        "https://my-json-server.typicode.com/dmitrijt9/book-api-mock/books"
+      );
+      setBooksData(res.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
     if (!state.authorizationToken) {
       navigate("/");
+    } else {
+      getBooksData();
     }
-    setIsLoading(false);
   }, [state.authorizationToken]);
 
   if (isLoading)
@@ -27,7 +44,21 @@ const Home = () => {
       </Box>
     );
 
-  return <div>Welcome to the Book Store</div>;
+  return (
+    <div>
+      {booksData.map((book) => (
+        <div key={book.id}>
+          <p>ID: {book.id}</p>
+          <p>Author ID: {book.author_id}</p>
+          <p>Title: {book.title}</p>
+          <p>Cover Img: {book.cover_image}</p>
+          <p>Pages: {book.pages}</p>
+          <p>Release Date: {book.releaseDate}</p>
+          <p>ISBN: {book.isbn}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
