c9c04c7690eff3b8787a6799035dcf16ef28a87c
prakash_yash <myashwanth096@gmail.com>
2023-04-08 11:13:46 +0530

    adding mockdata and navigating to home page if user is already logged in

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useMediaQuery } from "@mui/material";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import LoginForm from "../LoginForm/LoginForm";
 import SignupForm from "../SignupForm/SignupForm";
+import { UserContext } from "../../store/contextStore";
+import { addAuthToken } from "../../store/user/actions";
+import { SET_AUTHORIZATION_TOKEN } from "../../store/user/constants";
 
 export default function LoginPage() {
   const [displaySignup, setDisplaySignup] = useState(false);
+  const [_, dispatch] = useContext(UserContext);
   const SmMatches = useMediaQuery((theme) => theme.breakpoints.down("sm"));
 
+  const navigate = useNavigate();
+
   const displayingSignup = (value) => setDisplaySignup(value);
+
+  useEffect(() => {
+    const token = sessionStorage.getItem("Auth_Token");
+    if (token) {
+      dispatch(
+        addAuthToken({
+          type: SET_AUTHORIZATION_TOKEN,
+          token,
+        })
+      );
+      navigate("/home");
+    }
+  }, []);
 
   return (
     <Card sx={{ display: `${SmMatches ? "block" : "flex"}` }}>
